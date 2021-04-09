0f108f1ec25c26f677aede6762a4f5480b103933
kiang <kiange@gmail.com>
2021-04-10 00:17:36 +0800

    refetching data and saving in pretty format

--- scripts/01_fetch.php
+++ scripts/01_fetch.php
@@ -19,59 +19,57 @@ DashboardAjax/GetAgesAccDataStatistics?City=[row0]&Area=[row1];ALL,ALL
 DashboardAjax/GetAgesAccDataStatisticsNear12Month?City=[row0]&Area=[row1];ALL,ALL 
 */
 $dataPath = dirname(__DIR__) . '/raw/GetCitiesAreaAccDataStatistics';
-$options = ['ALL', '基隆市', '臺北市', '新北市', '桃園市', '新竹市', '新竹縣', '苗栗縣', '臺中市', '彰化縣', '南投縣', '雲林縣', '嘉義市', '嘉義縣',
-'臺南市', '高雄市', '屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '澎湖縣', '金門縣', '連江縣'];
+$options = [
+    'ALL', '基隆市', '臺北市', '新北市', '桃園市', '新竹市', '新竹縣', '苗栗縣', '臺中市', '彰化縣', '南投縣', '雲林縣', '嘉義市', '嘉義縣',
+    '臺南市', '高雄市', '屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '澎湖縣', '金門縣', '連江縣'
+];
 $baseUrl = 'https://roadsafety.tw/motcgisDashboard/api/DashboardAjax/GetCitiesAreaAccDataStatistics?';
-for($y = 98; $y <= 110; $y++) {
-    for($m = 1; $m <= 12; $m++) {
-        if($y === 110 && $m > 1) {
+for ($y = 98; $y <= 110; $y++) {
+    for ($m = 1; $m <= 12; $m++) {
+        if ($y === 110 && $m > 1) {
             continue;
         }
         $ty = str_pad($y, 3, '0', STR_PAD_LEFT);
         $tm = str_pad($m, 2, '0', STR_PAD_LEFT);
-        foreach($options AS $option) {
+        foreach ($options as $option) {
             $optionPath = $dataPath . '/' . $option;
-            if(!file_exists($optionPath)) {
+            if (!file_exists($optionPath)) {
                 mkdir($optionPath, 0777, true);
             }
             $targetFile = "{$optionPath}/{$ty}_{$tm}.json";
-            if(!file_exists($targetFile)) {
-                $city = urlencode($option);
-                file_put_contents($targetFile, file_get_contents("{$baseUrl}City={$city}&Cyear={$ty}%E5%B9%B4&Month={$tm}%E6%9C%88&Type=30%E6%97%A5%E6%AD%BB%E4%BA%A1%E4%BA%BA%E6%95%B8"));
-            }
+            $city = urlencode($option);
+            $json = json_decode(file_get_contents("{$baseUrl}City={$city}&Cyear={$ty}%E5%B9%B4&Month={$tm}%E6%9C%88&Type=30%E6%97%A5%E6%AD%BB%E4%BA%A1%E4%BA%BA%E6%95%B8"));
+            file_put_contents($targetFile, json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
         }
-        
     }
 }
 
 $dataPath2 = dirname(__DIR__) . '/raw/GetCitiesAccData_EachYM';
 $baseUrl2 = 'https://roadsafety.tw/motcgisDashboard/api/DashboardAjax/GetCitiesAccData_EachYM?';
-foreach($options AS $option) {
+foreach ($options as $option) {
     $optionPath = $dataPath2 . '/' . $option;
-    if(!file_exists($optionPath)) {
+    if (!file_exists($optionPath)) {
         mkdir($optionPath, 0777, true);
     }
     $targetFile = "{$optionPath}/data.json";
-    if(!file_exists($targetFile)) {
-        $city = urlencode($option);
-        file_put_contents($targetFile, file_get_contents("{$baseUrl2}City={$city}&Area=ALL"));
-    }
+    $city = urlencode($option);
+    $json = json_decode(file_get_contents("{$baseUrl2}City={$city}&Area=ALL"));
+    file_put_contents($targetFile, json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
 }
 
 $dataPath3 = dirname(__DIR__) . '/raw/GetStaticData';
 $topics = ['年齡與運具', '碰撞型態與位置與肇事原因', '年齡與碰撞型態與位置', '碰撞型態與位置與運具', '肇事原因與運具'];
 $baseUrl3 = 'https://roadsafety.tw/motcgisDashboard/api/DashboardAjax/GetStaticData?';
-foreach($topics AS $topic) {
+foreach ($topics as $topic) {
     $uTopic = urlencode($topic);
-    foreach($options AS $option) {
+    foreach ($options as $option) {
         $optionPath = $dataPath3 . '/' . $option;
-        if(!file_exists($optionPath)) {
+        if (!file_exists($optionPath)) {
             mkdir($optionPath, 0777, true);
         }
         $targetFile = "{$optionPath}/{$topic}.json";
-        if(!file_exists($targetFile)) {
-            $city = urlencode($option);
-            file_put_contents($targetFile, file_get_contents("{$baseUrl3}Topic={$uTopic}&City={$city}"));
-        }
+        $city = urlencode($option);
+        $json = json_decode(file_get_contents("{$baseUrl3}Topic={$uTopic}&City={$city}"));
+        file_put_contents($targetFile, json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
     }
 }
